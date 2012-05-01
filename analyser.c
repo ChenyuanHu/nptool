23b58c538df600c41afdeb9e32893823bec8495c
Chenyuan Hu <hcyjob@gmail.com>
2012-05-02 00:12:52 +0800

    add arp analyse

--- analyser.c
+++ analyser.c
@@ -8,6 +8,11 @@
 #include "analyser.h"
 #include "proto_headers.h"
 
+#define UPDATE_DST 1
+#define UPDATE_SRC 2
+#define UPDATE_PRO 4
+#define UPDATE_INFO 8
+
 static void free_analytree(struct pkt_analytree *analytree)
 {
 	if (analytree == NULL)
@@ -129,7 +134,7 @@ static int tcp_dump(struct pkt_summary *summ, struct pkt_analytree **analytree,
 
 	snprintf(summ->proto, STR_PROTO_LEN, "tcp");
 
-	ret = 0;
+	ret = UPDATE_PRO;
 	
 	*analytree = atree;
 	return ret;	
@@ -203,7 +208,160 @@ static int udp_dump(struct pkt_summary *summ, struct pkt_analytree **analytree,
 
 	snprintf(summ->proto, STR_PROTO_LEN, "udp");
 
+	ret = UPDATE_PRO;
+	
+	*analytree = atree;
+	return ret;	
+err:
+	free_analytree(atree);
+	*analytree = NULL;
+	return -1;
+
+}
+static int arp_dump(struct pkt_summary *summ, struct pkt_analytree **analytree,
+		const unsigned char *data, unsigned int size)
+{
+	struct pkt_analytree *atree;
+	struct pkt_analytree *child;
+	struct npt_arp_hdr *hdr;
+
+
+	uint16_t hrd;         /* format of hardware address */
+	uint16_t pro;         /* format of protocol address */
+	uint8_t  hln;         /* length of hardware address */
+	uint8_t  pln;         /* length of protocol addres */
+	uint16_t op;          /* operation type */
+
+	char *htype;
+	char *optype;
+	int ret;
 	ret = 0;
+
+	if ((atree = malloc(sizeof(*atree))) == NULL) {
+		fprintf(stderr, "malloc err %s\n", __FUNCTION__);
+		*analytree = NULL;
+		return -1;
+	}
+	bzero(atree, sizeof(*atree));
+
+	hdr = (struct npt_arp_hdr *)data;
+
+	if (size < NPT_ARP_H) 
+		goto err;
+
+	hrd = ntohs(hdr->ar_hrd);
+	pro = ntohs(hdr->ar_pro);
+	hln = hdr->ar_hln;
+	pln = hdr->ar_pln;
+	op = ntohs(hdr->ar_op);
+
+	switch (hrd) {
+	case ARPHRD_NETROM:
+		htype = "from KA9Q: NET/ROM pseudo";
+		break;
+	case ARPHRD_ETHER:
+		htype = "Ethernet 10Mbps";
+		break;
+	case ARPHRD_EETHER:
+		htype = "Experimental Ethernet";
+		break;
+	case ARPHRD_AX25:
+		htype = "AX.25 Level 2";
+		break;
+	case ARPHRD_PRONET:
+		htype = "PROnet token ring";
+		break;
+	case ARPHRD_CHAOS:
+		htype = "Chaosnet";
+		break;
+	case ARPHRD_IEEE802:
+		htype = "IEEE 802.2 Ethernet/TR/TB";
+		break;
+	case ARPHRD_ARCNET:
+		htype = "ARCnet";
+		break;
+	case ARPHRD_APPLETLK:
+		htype = "APPLEtalk";
+		break;
+	case ARPHRD_LANSTAR:
+		htype = "Lanstar";
+		break;
+	case ARPHRD_DLCI:
+		htype = "Frame Relay DLCI";
+		break;
+	case ARPHRD_ATM:
+		htype = "ATM";
+		break;
+	case ARPHRD_METRICOM:
+		htype = "Metricom STRIP (new IANA id)";
+		break;
+	case ARPHRD_IPSEC:
+		htype = "IPsec tunnel";
+		break;
+	}
+	switch (op) {
+	case ARPOP_REQUEST:
+		optype = "req to resolve address";
+		break;
+	case ARPOP_REPLY:
+		optype = "resp to previous request";
+		break;
+	case ARPOP_REVREQUEST:
+		optype = "req protocol address given hardware";
+		break;
+	case ARPOP_REVREPLY:
+		optype = "resp giving protocol address";
+		break;
+	case ARPOP_INVREQUEST:
+		optype = "req to identify peer";
+		break;
+	case ARPOP_INVREPLY:
+		optype = "resp identifying peer";
+		break;
+	}
+
+	strncpy(atree->comment, "arp protocol", STR_COMMENT_LEN);
+	atree->next = NULL;
+	if ((atree->child = malloc(sizeof(*atree))) == NULL) {
+		fprintf(stderr, "malloc err %s\n", __FUNCTION__);
+		goto err;
+	}
+	child = atree->child;
+	snprintf(child->comment, STR_INFO_LEN, "Hardware type: %s", htype);
+	child->child = NULL;
+	if ((child->next = malloc(sizeof(*child))) == NULL) {
+		fprintf(stderr, "malloc err %s\n", __FUNCTION__);
+		goto err;
+	}
+	child = child->next;
+	snprintf(child->comment, STR_INFO_LEN, "Protocol type: %d", pro);
+	child->child = NULL;
+	if ((child->next = malloc(sizeof(*child))) == NULL) {
+		fprintf(stderr, "malloc err %s\n", __FUNCTION__);
+		goto err;
+	}
+	child = child->next;
+	snprintf(child->comment, STR_INFO_LEN, "Hardware address length: %u", hln);
+	child->child = NULL;
+	if ((child->next = malloc(sizeof(*child))) == NULL) {
+		fprintf(stderr, "malloc err %s\n", __FUNCTION__);
+		goto err;
+	}
+	child = child->next;
+	snprintf(child->comment, STR_INFO_LEN, "Protocol address length: %u", pln);
+	child->child = NULL;
+	if ((child->next = malloc(sizeof(*child))) == NULL) {
+		fprintf(stderr, "malloc err %s\n", __FUNCTION__);
+		goto err;
+	}
+	child = child->next;
+	snprintf(child->comment, STR_INFO_LEN, "Operation Type : %s", optype);
+	child->child = NULL;
+	child->next = NULL;
+
+	snprintf(summ->proto, STR_PROTO_LEN, "arp");
+
+	ret = UPDATE_PRO;
 	
 	*analytree = atree;
 	return ret;	
@@ -303,22 +461,27 @@ static int ip_dump(struct pkt_summary *summ, struct pkt_analytree **analytree,
 		ret = tcp_dump(summ, &(atree->next), data + NPT_IPV4_H, size - NPT_IPV4_H);
 		break;
 	}
-	snprintf(summ->dst, STR_DST_LEN, "%u.%u.%u.%u",
-			hdr->ip_dst[0], hdr->ip_dst[1], hdr->ip_dst[2], hdr->ip_dst[3]);
 
-	snprintf(summ->src, STR_SRC_LEN, "%u.%u.%u.%u",
-			hdr->ip_src[0], hdr->ip_src[1], hdr->ip_src[2], hdr->ip_src[3]);
+	if (ret < 0 || ((ret & UPDATE_DST) != UPDATE_DST))
+		snprintf(summ->dst, STR_DST_LEN, "%u.%u.%u.%u",
+				hdr->ip_dst[0], hdr->ip_dst[1], hdr->ip_dst[2], hdr->ip_dst[3]);
 
-	if (hdr->ip_dst[0] == 0xff && hdr->ip_dst[1] == 0xff &&
-			hdr->ip_dst[2] == 0xff && hdr->ip_dst[3] == 0xff) {
-		snprintf(summ->info, STR_INFO_LEN, "%s broadcast", summ->src);
-	} else {
-		snprintf(summ->info, STR_INFO_LEN, "%s -> %s", summ->src, summ->dst);
+	if (ret < 0 || ((ret & UPDATE_SRC) != UPDATE_SRC))
+		snprintf(summ->src, STR_SRC_LEN, "%u.%u.%u.%u",
+				hdr->ip_src[0], hdr->ip_src[1], hdr->ip_src[2], hdr->ip_src[3]);
+
+	if (ret < 0 || ((ret & UPDATE_INFO) != UPDATE_INFO)) {
+		if (hdr->ip_dst[0] == 0xff && hdr->ip_dst[1] == 0xff &&
+				hdr->ip_dst[2] == 0xff && hdr->ip_dst[3] == 0xff) {
+			snprintf(summ->info, STR_INFO_LEN, "%s broadcast", summ->src);
+		} else {
+			snprintf(summ->info, STR_INFO_LEN, "%s -> %s", summ->src, summ->dst);
+		}
 	}
-	if (ret != 0) {
+	if (ret < 0 || ((ret & UPDATE_PRO) != UPDATE_PRO))
 		snprintf(summ->proto, STR_PROTO_LEN, "ip");
-	}
-	ret = 0;
+
+	ret = UPDATE_DST | UPDATE_SRC | UPDATE_INFO | UPDATE_PRO;
 	
 	*analytree = atree;
 	return ret;	
@@ -392,19 +555,27 @@ static int ethernet_dump(struct pkt_summary *summ, struct pkt_analytree **analyt
 	case ETHERTYPE_IP:
 		ret = ip_dump(summ, &(atree->next), data + NPT_ETH_H, size - NPT_ETH_H);
 		break;
+	case ETHERTYPE_ARP:
+		ret = arp_dump(summ, &(atree->next), data + NPT_ETH_H, size - NPT_ETH_H);
+		break;
 	}
-	if (ret != 0) {
+
+	if (ret < 0 || ((ret & UPDATE_DST) != UPDATE_DST))
 		snprintf(summ->dst, STR_DST_LEN, "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x",
 				hdr->ether_dhost[0], hdr->ether_dhost[1], 
 				hdr->ether_dhost[2], hdr->ether_dhost[3], 
 				hdr->ether_dhost[4], hdr->ether_dhost[5]);
 
+	if (ret < 0 || ((ret & UPDATE_SRC) != UPDATE_SRC))
 		snprintf(summ->src, STR_SRC_LEN, "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x",
 				hdr->ether_shost[0], hdr->ether_shost[1], 
 				hdr->ether_shost[2], hdr->ether_shost[3], 
 				hdr->ether_shost[4], hdr->ether_shost[5]);
 
+	if (ret < 0 || ((ret & UPDATE_PRO) != UPDATE_PRO))
 		snprintf(summ->proto, STR_PROTO_LEN, "ethernet");
+
+	if (ret < 0 || ((ret & UPDATE_INFO) != UPDATE_INFO)) {
 		if (hdr->ether_dhost[0] == 0xff && hdr->ether_dhost[1] == 0xff &&
 				hdr->ether_dhost[2] == 0xff && hdr->ether_dhost[3] == 0xff &&
 				hdr->ether_dhost[4] == 0xff && hdr->ether_dhost[5] == 0xff) {
@@ -414,7 +585,7 @@ static int ethernet_dump(struct pkt_summary *summ, struct pkt_analytree **analyt
 					summ->src, summ->dst);
 		}
 	}
-	ret = 0;
+	ret = UPDATE_DST | UPDATE_SRC | UPDATE_INFO | UPDATE_PRO;
 	
 	*analytree = atree;
 	return ret;	
@@ -443,6 +614,8 @@ int pkt_analyse(struct pkt_summary **summary, struct pkt_analytree **analytree,
 	summ->caplen = caplen;
 
 	ret = ethernet_dump(summ, &atree, data, caplen);
+	if (ret > 0)
+		ret = 0;
 
 	*summary = summ;
 	*analytree = atree;
